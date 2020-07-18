7b07b403edd3a007be7a9ff3b7084aa849ed5194
Cerebral Nomad <me@example.com>
2020-07-18 02:46:53 -0400

    Changed the default password length from 12 to 23

--- newpass.py
+++ newpass.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(
         epilog = ('''\
 examples:
 
-    newpass - generates a 12 character alphanumeric password
+    newpass - generates a 23 character alphanumeric password
     newpass 20 - generates a 20 character alphanumeric password
     newpass -s 25 - generates a 25 character password that can include all symbols
     newpass -l 15 - generates a 15 character password that can include symbols !@#$%&*
@@ -21,20 +21,21 @@ examples:
     precedence and the -l is ignored
 
     Passwords created always start with a letter, contain at least
-    one capital and one lowercase letter and at least 3 digits
+    one capital and one lowercase letter and at least 3 digits and
+    will not include a capital O to avoid confusion with zero
                 ''')
                 )
-parser.add_argument('length', type = int, nargs='?', default = 12, help = 'Desired password length')
-parser.add_argument('--symbol', '-s', help = 'Include symbols in the password', action='store_true')
+parser.add_argument('length', type = int, nargs='?', default = 23, help = 'Desired password length. If not specified, default is 23')
+parser.add_argument('--symbol', '-s', help = 'Include all symbols in the password', action='store_true')
 parser.add_argument('--limit', '-l', default=False, help = "Limit symbols used to !@#$%%&*", action='store_true')
 args = parser.parse_args()
 
 punc = '!"#$%&\'()*+,-:;=?@[\]^_{}~'
 limit_punc = '!@#$%*&!@#$%&*'
 length = args.length
-alphabet = string.ascii_letters + string.digits
-with_symbols = string.ascii_letters + string.digits + punc
-limit_symbols = string.ascii_letters + string.digits + limit_punc
+alphabet = string.ascii_letters[0:40] + string.ascii_letters[41:] + string.digits
+with_symbols = alphabet + punc
+limit_symbols = alphabet + limit_punc
 
 if not args.symbol and not args.limit:
     while True:
