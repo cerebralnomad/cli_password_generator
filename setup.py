fe56db538261c64452e68d5128d11d7f73825981
Cerebral Nomad <me@example.com>
2020-07-25 14:18:55 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='newpass',
    version='1.0',
+   packages=find_packages(),
+   scripts=['newpass.py'],
    description='Generate random passwords from the command line',
    author='Cerebral Nomad',
    author_email='cerebralnomad@protonmail.com',
-   packages=['newpass'],
-   scripts=['newpass.py'],
+   
 )
